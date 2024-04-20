85251722eb8e2ad2f41a6e4f7a878692ad0ed5e8
Freedteck <freedteck@gmail.com>
2024-04-20 17:07:41 +0100

    add wagmi

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,22 +1,31 @@
 import { Link } from "react-router-dom";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Navbar = () => {
-    return ( 
-        <header className="navbar-header">
-            <nav className="container">
-                <h1 className="logo">B<span>C</span>D</h1>
-                <ul className="nav-links">
-                    <li className="active"><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/explore">Explore</Link></li>
-                    {/* <li><Link to="/contact">How it Works</Link></li> */}
-                </ul>
-                <div>
-                    <Link to="/connect" className="btn connect">Connect</Link>
-                </div>
-            </nav>
-        </header>
-     );
-}
- 
+  return (
+    <header className="navbar-header">
+      <nav className="container">
+        <h1 className="logo">
+          B<span>C</span>D
+        </h1>
+        <ul className="nav-links">
+          <li className="active">
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/explore">Explore</Link>
+          </li>
+          {/* <li><Link to="/contact">How it Works</Link></li> */}
+        </ul>
+        <div>
+          <ConnectButton />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
 export default Navbar;
